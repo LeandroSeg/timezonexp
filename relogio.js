5e85aa9c9f30615e541662c189f7fdd2e18b38ab
LEANDRO SEGATTI <dragonavenger@gmail.com>
2023-10-06 13:51:17 -0300

    otimização na leitura do código

--- relogio.js
+++ relogio.js
@@ -1,17 +1,12 @@
-
-function Start(areasRelogios) {
+function Iniciar(areasRelogios) {
   for( let element of areasRelogios) {         
     
     let timeZone=element.getAttribute('timeZone')
-    let pais=element.getAttribute('pais')
-    
-    let iconsol  =element.querySelector(".iconsol")
-    console.log(iconsol)
-    let iconlua  =element.querySelector(".iconlua")
-    
+    let pais=element.getAttribute('pais')    
+    let iconsol  =element.querySelector(".iconsol")    
+    let iconlua  =element.querySelector(".iconlua")    
     let bandeirapais=element.querySelector(".nomepais")
-    bandeirapais.innerHTML=pais
-    
+
     let spanHora = element.querySelector(".hora")
     let spanMinuto = element.querySelector(".minuto")
     let spanSegundo= element.querySelector(".segundo")
@@ -31,44 +26,17 @@ function Start(areasRelogios) {
 
         } )
       
-      let hour    =horatempo.substring(12,14)
-      let minute  =horatempo.substring(15,17)
-      let second  =horatempo.substring(18,20)
-      let day     =horatempo.substring(0,2)
-      let month   =horatempo.substring(3,5)
-      let year    =horatempo.substring(6,10)
-
-      
-      if ((hour>=0 && hour<=7) || (hour>=18 && hour<=23))
-      {        
-        iconsol.style.display="none";
-        iconlua.style.display="block";
-        
-        
-      }
-      else
-      {        
-        iconsol.style.display="block";
-        iconlua.style.display="none";
-      }
+      let { exHora, exAno, exMes, exDia, exMinuto, exSegundo } = extrairComponentesDeData(horatempo)      
+      atualizarIndicadoresVisuaisDiaNoite(exHora, iconsol, iconlua)
 
       let horaTempoTimeZone = new Date(
-        parseInt(year), parseInt(month)-1, parseInt(day), parseInt(hour), parseInt(minute),parseInt(second));
+        parseInt(exAno), parseInt(exMes)-1, parseInt(exDia), parseInt(exHora), parseInt(exMinuto),parseInt(exSegundo));      
 
-      let horaDif =  Math.round( (horaTempoTimeZone  - horatempoAtual)/1000/60/60 )
+      atualizarCamposHMS(spanHora, exHora, spanMinuto, exMinuto, spanSegundo, exSegundo)
 
-      spanHora.innerHTML    = hour
-      spanMinuto.innerHTML  = minute
-      spanSegundo.innerHTML = second
-
-      if (horaDif==0)
-      {
-        spanHoraDif.innerHTML = ""
-      }
-      else  
-      {
-        spanHoraDif.innerHTML = (horaDif>0 ? "+":"")+horaDif
-      }
+      atualizarIndicadorHoras(
+        calcularDiferencaFusoHorario(horaTempoTimeZone, horatempoAtual)
+        , spanHoraDif)
 
     }  
     ,1000)
@@ -76,8 +44,45 @@ function Start(areasRelogios) {
   
 }
 
+function atualizarCamposHMS(spanHora, exHora, spanMinuto, exMinuto, spanSegundo, exSegundo) {
+  spanHora.innerHTML = exHora
+  spanMinuto.innerHTML = exMinuto
+  spanSegundo.innerHTML = exSegundo
+}
 
+function calcularDiferencaFusoHorario(horaTempoTimeZone, horatempoAtual) {
+  return Math.round((horaTempoTimeZone - horatempoAtual) / 1000 / 60 / 60)
+}
 
-function fmtZerosEsquerdaText(num, totalLength) {
-  return String(num).padStart(totalLength, '0');
+function extrairComponentesDeData(horatempo) {
+  let exHora = horatempo.substring(12, 14)
+  let exMinuto = horatempo.substring(15, 17)
+  let exSegundo = horatempo.substring(18, 20)
+  let exDia = horatempo.substring(0, 2)
+  let exMes = horatempo.substring(3, 5)
+  let exAno = horatempo.substring(6, 10)
+  return { exHora, exAno, exMes, exDia, exMinuto, exSegundo }
+}
+
+function atualizarIndicadorHoras(horaDif, spanHoraDif) {
+  if (horaDif == 0) {
+    spanHoraDif.innerHTML = ""
+  }
+
+  else {
+    spanHoraDif.innerHTML = (horaDif > 0 ? "+" : "") + horaDif
+  }
+}
+
+function atualizarIndicadoresVisuaisDiaNoite(exHora, iconsol, iconlua) {
+  if ((exHora >= 0 && exHora <= 7) || (exHora >= 18 && exHora <= 23)) {
+    iconsol.style.display = "none"
+    iconlua.style.display = "block"
+
+  }
+
+  else {
+    iconsol.style.display = "block"
+    iconlua.style.display = "none"
+  }
 }
